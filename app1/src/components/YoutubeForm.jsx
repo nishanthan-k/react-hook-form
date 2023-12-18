8d3d0952ef4734e9d9d877d6eff6232588a2bd63
nishanthan-k <nishanthank9102@gmail.com>
2023-12-18 21:25:46 +0530

    changed the css and displayed the error

--- app1/src/components/YoutubeForm.jsx
+++ app1/src/components/YoutubeForm.jsx
@@ -1,50 +1,66 @@
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
-import "./youtubeform.css"
-
-var renderCount = 0;
+import "./youtubeform.css";
 
 const YoutubeForm = () => {
-  const form = useForm();
-  const { register, control, handleSubmit } = form;
+	const form = useForm();
+	const { register, control, handleSubmit, formState } = form;
+	const { errors } = formState;
 
-  const onSubmit = (data) => {
-    console.log(data);
-  }
+	const onSubmit = (data) => {
+		console.log(data);
+	};
 
-  renderCount++;
+	// console.log(errors);
 
-  return (
-    <div>
-      <h2>Render Count : {renderCount / 2}</h2>
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <label htmlFor="username">Username
-          <input
-            type="text"
-            id="username"
-            autoFocus
-            {...register("username", { required: 'Username is required' })}
-          /></label>
-        <label htmlFor="email">Email
-          <input
-            type="text"
-            id="email"
-            {...register("email")}
-          />
-        </label>
-        <label htmlFor="channel">Channel
-          <input
-            type="text"
-            id="channel"
-            {...register("channel")}
-          />
-        </label>
+	return (
+		<div className="container">
+			<form onSubmit={handleSubmit(onSubmit)} noValidate>
+				<div className="form-field">
+					<div className="form-input">
+						<label htmlFor="username">Username</label>
+						<input
+							type="text"
+							id="username"
+							autoFocus
+							{...register("username", { required: "Username is required" })}
+						/>
+					</div>
+					<p className="error">{errors.username?.message}</p>
+				</div>
+				<div className="form-field">
+					<div className="form-input">
+						<label htmlFor="email">Email</label>
+						<input
+							type="text"
+							id="email"
+							{...register("email", {
+								pattern: {
+									value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+									message: "Enter valid email id",
+								},
+							})}
+						/>
+					</div>
+					<p className="error">{errors.email?.message}</p>
+				</div>
+				<div className="form-field">
+					<div className="form-input">
+						<label htmlFor="channel">Channel</label>
+						<input
+							type="text"
+							id="channel"
+							{...register("channel", { required: "Channel name is required" })}
+						/>
+					</div>
+					<p className="error">{errors.channel?.message}</p>
+				</div>
 
-        <button>Submit</button>
-      </form>
-      <DevTool control={control} />
-    </div>
-  )
-}
+				<button>Submit</button>
+			</form>
+			<DevTool control={control} />
+		</div>
+	);
+};
 
-export default YoutubeForm
+export default YoutubeForm;
